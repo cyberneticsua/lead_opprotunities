c54691bff8875d60fa230c5a16b42871ee5e7f8a
cyberneticsua <igor.vinnychuk@gmail.com>
2018-01-31 13:56:16 +0200

    Restrict stage changing without product code

--- models/crm_stage_control.py
+++ models/crm_stage_control.py
@@ -9,14 +9,32 @@
 
 from odoo import models, fields, api
 from datetime import date
+from odoo.exceptions import UserError
+from odoo.exceptions import Warning
 
 class ActivityControl (models.Model):
     _inherit='crm.lead'
     
     @api.onchange('stage_id')
     def _onchange_stage_id(self):
+        ####Помилка при перенесенні деталі без коду
+        my_crm=self.env['crm.lead'].search([('id','=',self._origin.id)])
+        qty_sales = 0
+        for data in my_crm.pdt_line:
+            if (data.default_code):
+                qty_sales += 1
+                # raise Warning (data.default_code)
+
+        # raise Warning (qty_sales)        
+        if (not qty_sales):
+            raise Warning(('Ви намагаєтесь перенести деталь без коду')) 
+
+
         values = self._onchange_stage_id_values(self.stage_id.id)
         self.update(values)
+        
         #код для оновлення pdt_line
         my_pdt_line = self.env['crm.product_line'].search([('child_opportunity', '=', int(self._origin.id))])
         my_pdt_line.write({'stage_name':self.stage_id.name})
+        
+        
